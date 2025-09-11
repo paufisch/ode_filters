a264813751cb6572d233520ac363107c702d878a
Paul Fischer <fischerpaul99@gmail.com>
2025-09-11 18:09:20 +0200

    adds stable square root gaussian inference functions

--- src/sqr_gaussian_inference.py
+++ src/sqr_gaussian_inference.py
@@ -1,3 +1,4 @@
+from ast import Lambda
 import numpy as np
 
 def conditioning(A, x, b, Q):
@@ -5,22 +6,26 @@ def conditioning(A, x, b, Q):
     return A @ x + b, Q
 
 
-def marginalization(A, b, Q, mu, Sigma):
+def sqr_marginalization(A, b, Q_sqr, mu, Sigma_sqr):
 
     mu_z = A @ mu + b
-    Sigma_z = A @ Sigma @ A.T + Q
+    C = np.concatenate([A @ Sigma_sqr, Q_sqr], axis=0)
+    _, Sigma_z_sqr = np.linalg.qr(C)
 
-    return mu_z, Sigma_z
+    return mu_z, Sigma_z_sqr
 
 
-def inversion(A, b, Q, mu, Sigma, z):
+def sqr_inversion(A, b, Q_sqr, mu, Sigma_sqr, z):
 
-    mu_z, Sigma_z = marginalization(A, b, Q, mu, Sigma)
+    mu_z, Sigma_z_sqr = sqr_marginalization(A, b, Q_sqr, mu, Sigma_sqr)
+    Sigma = Sigma_sqr.T @ Sigma_sqr
+    Sigma_z = Sigma_z_sqr.T @ Sigma_z
     G = Sigma @ A.T @ np.linalg.inv(Sigma_z)
     d = mu - G @ mu_z
-    #Lambda = Sigma - G @ Sigma_z @ G.T
-    B = np.eye((G@A).shape[0]) - G@A
-    Lambda = B @ Sigma @ B.T + G @ Q @ G.T #Josephson Form
     
-    return conditioning(G, z, d, Lambda)
+    B = np.eye((G@A).shape[0]) - G@A
+    C = np.concatenate([Sigma_sqr @ B.T, Q_sqr @ G.T])
+    _, Lambda_sqr = np.linalg.qr(C)
+    
+    return G, d, Lambda_sqr
 
